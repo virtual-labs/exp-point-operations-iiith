199e97c251cdb1c71a3a0e53bfd1e6b04792ae89
sravanthimodepu <sravanthimodepu@gmail.com>
2021-10-04 17:55:16 +0530

    fixed simulation, assignment and added images

--- experiment/simulation/point.php
+++ experiment/simulation/point.php
@@ -65,7 +65,7 @@ if(isset($_GET["opt"])) {
 <!-- This file was originally generated at http://psd2cssonline.com on September 16, 2010, 10:04 am -->
 <!-- psd2css Online version 1.85 -->
 
-<title>Virtual Lab in Image Processing</title>
+<!--<title>Virtual Lab in Image Processing</title>-->
 
 <!-- Some META tags to help with Search Engine Optimization.  Please 
 note however that META tags are NOT a magic bullet to get your web page
@@ -478,11 +478,11 @@ $("button3","#zoom").button();
     <div id="Layer-3" class="TopBar_jpg"  >
       <img src="images/Layer-3.jpg" width="894" height="96" alt="TopBar" />
       <!-- This is 'IIIT' -->
-      <div id="Layer-6" class="IIIT"  >
+     <!-- <div id="Layer-6" class="IIIT"  >
         <img src="images/iiit.png" width="100" height="70" alt="IIIT" class="pngimg" />
-	</div>
+	</div>-->
 	
-			<div id="topMenu">
+		<!--	<div id="topMenu">
 			<h1 style="text-align: center;">Virtual Lab for IMAGE PROCESSING</h1>
 			<div class="menu">
 <div class="home">
@@ -511,7 +511,7 @@ $("button3","#zoom").button();
 <li><a href="summary.php" target="_blank" >Summary</a>
 </li>
 </ul>
-</div>
+</div>-->
 			</div>
 			
 
