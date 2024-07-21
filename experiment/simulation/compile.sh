08030fe0cd71877a93c1ca67a9dc106df1afb08c
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-07-22 00:58:55 +0530

    Modified simulation and assignment

--- experiment/simulation/compile.sh
+++
@@ -1,7 +0,0 @@
-export OPENCV_FLAGS=$(pkg-config --cflags --libs opencv4)
-
-g++ codes/im_point.cpp $OPENCV_FLAGS -o execs/point.out
-g++ codes/im_colour.cpp $OPENCV_FLAGS -o execs/colour.out 
-
-cp execs/point.out ../assignment/execs/point.out
-cp execs/colour.out ../assignment/execs/colour.out
